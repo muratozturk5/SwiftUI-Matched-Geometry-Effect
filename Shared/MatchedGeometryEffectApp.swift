ad090b96c302383e7e5011edf8f4978c1878c83e
Murat ÖZTÜRK <murat318ozturk@gmail.com>
2022-07-03 13:12:49 +0300

    Initial Commit

---
+++ Shared/MatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectApp.swift
+//  Shared
+//
+//  Created by Macbook on 3.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
